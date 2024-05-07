b57c3f6b6f6720b83834957248f63e001ce5c60c
Patricia 607413324 <patriciapatriciaariasburdeos@gmail.com>
2024-05-07 21:47:54 +0200

    Modificando ubicación de READM.md

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(express.json())
 
 app.use("/products",require("./routes/products"))
 app.use("/categories",require("./routes/categories"))
-
 app.use("/users",require("./routes/user"));
+//app.use('/orders',require('./routes/order'));
 
 
 //app.use(typeError)
