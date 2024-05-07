572d1f35b38d3f6db95d68071622962cc3df441f
Patricia 607413324 <patriciapatriciaariasburdeos@gmail.com>
2024-05-07 17:39:43 +0200

    endpoints pedidos

---
+++ controller/OrderController.js
@@ -0,0 +1,26 @@
+const { Order, Product } = require('../models/index');
+const OrderController = {
+    async create (req, res) {
+        try {
+            req.body.UserId = req.user.id
+            const order = await Order.create(req.body);
+            order.addProduct(req.body.ProductId)
+            res.status(201).send({ msg: 'Pedido realizado con éxito', order });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({ msg: 'Error interno del servidor', err });
+        }
+    },
+    async getAll(req, res) {
+        try {
+          const orders = await Order.findAll({
+            include:[{ model: Product,attributes:["name"], through: { attributes: [] } }]
+          });
+          res.send(orders);
+        } catch (error) {
+          console.error(error);
+          res.status(500).send({ msg: 'Error interno del servidor', err });
+        }
+      },
+};
+module.exports = OrderController
---
+++ routes/order.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const OrderController = require('../controllers/OrderController');
+const { authentication } = require('../middleware/authentication');
+const router = express.Router();
+
+
+router.post('/',authentication,OrderController.create),
+router.get('/',authentication,OrderController.getAll),
+
+
+module.exports = router;
